ab1ff53aa3a25115e02376609560c554be670ac9
l4johnson <ljohnson15@zagmail.gonzaga.edu>
2021-03-17 11:18:10 -0700

    Project 9

---
+++ proj9/2-List.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+#include "2-List.h"
+
+List::List()
+{       
+ length = 0;
+ head = NULL;
+}
+
+
+List::~List()
+{
+}
+
+bool List::IsEmpty() const
+{
+	if(head == NULL)
+ 		return true;
+ 	else 
+ 		return false;
+}
+
+int List::GetLength() const
+{
+		
+ return length;
+}
+
+void List::PutItemH(const itemType newItem)
+{
+ node* tmp = new node;
+ tmp->item = newItem;
+ tmp->next = head;
+ head = tmp;
+ length++;
+ tmp = NULL;
+}
+
+itemType List::GetItemH() const
+{
+ return head->item;
+}
+
+void List::DeleteItemH()
+{
+	node* point = head;
+	
+	head = point->next;
+	
+	length--;
+	
+}
+
+	
+void List::Print() const
+{
+	node* point = head;
+	
+	for(int i = 0; i < length; i++)
+	{
+		cout << point->item << endl;
+		point = point->next;
+	}
+}
+
+int List::Find(const itemType item) const
+{
+	node* point = head;
+	
+	for(int i = 0; i < length; i++)
+		if(point->item == item)
+			return i;
+		else 
+			point = point->next;
+ return -1;
+}
+
+void List::DeleteItem(const int pos)
+{
+	node* point = head;
+	
+	for(int i = 0; i < pos; i++)
+		point = point->next;
+		
+	node* nextItem = point->next;
+	point = head;
+		
+	for(int i = 0; i < pos-1; i++)
+		point = point->next;
+		
+	point->next = nextItem;
+	length--;
+}
+
---
+++ proj9/2-ListTst.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+using namespace std;
+
+#include "2-List.h"
+
+int main()
+{
+	/*//// Test 1 - PutItemH & GetItemH
+ //Use of a static list 
+ List lst;
+ lst.PutItemH(77);
+ cout << lst.GetItemH() << endl;
+ 
+ for (int i = 0; i < 5; i++) 
+   lst.PutItemH(i);
+ lst.Print();
+ cout << endl;
+ //Use of a dynamic list
+ List* lst1 = new List;
+ for (int i = 0; i < 5; i++)
+   lst1->PutItemH(10*i);
+ lst1->Print();
+ cout << endl;
+
+ delete lst1; //necessary to invoke destructor on dynamic list
+ */
+ 
+ 
+/*/// Test 2 - IsEmpty
+	cout << "Test IsEmpty" << endl;
+  //Use of a static list 
+  List lst;
+  cout << "Is the list empty? " << lst.IsEmpty() << endl;
+  cout << "adding item to list..." << endl;
+  cout << "New list: ";
+  for (int i = 0; i < 5; i++) 
+  lst.PutItemH(i);
+  lst.Print();
+  cout << endl;
+  cout << "Is the list empty? " << lst.IsEmpty() << endl;
+ 
+  //Use of a dynamic list
+  List* lst1 = new List;
+  cout << "Is the list empty? " << lst1->IsEmpty() << endl;
+  cout << "adding item to list..." << endl;
+  for (int i = 0; i < 5; i++)
+		lst1->PutItemH(10*i);
+  cout << "New list: ";
+  lst1->Print();
+  cout << "Is the list empty? " << lst1->IsEmpty() << endl;
+  cout << endl;
+
+  delete lst1; //necessary to invoke destructor on dynamic list
+*///
+ 
+ 
+/*/// Test 3 - DeleteItemH
+	cout << "Test DeleteItemH" << endl;
+  //Use of a static list 
+  List lst;
+  cout << "List: ";
+  for (int i = 0; i < 5; i++) 
+    lst.PutItemH(i);
+  lst.Print();
+  cout << "Deleting first item..." << endl;
+  lst.DeleteItemH();
+  cout << "New List: ";
+  lst.Print();
+  cout << endl;
+  
+  //Use of a dynamic list
+  List* lst1 = new List;
+  cout << "List: " << endl;
+  for (int i = 0; i < 5; i++)
+   lst1->PutItemH(10*i);
+  lst1->Print();
+  cout << "Deleting first item..." << endl;
+  lst1->DeleteItemH();
+  cout << "New list: ";
+  lst1->Print();
+  cout << endl;
+
+  delete lst1; //necessary to invoke destructor on dynamic list
+*/
+ 
+ 
+/*/// Test 4 - GetLength
+	cout << "Test GetLength" << endl;
+  //Use of a static list 
+  List lst;
+  cout << "How long is the list?" << lst.GetLength() << endl;
+  cout << "adding item to list..." << endl;
+  cout << "New list: ";
+  for (int i = 0; i < 5; i++) 
+		lst.PutItemH(i);
+  lst.Print();
+  cout << endl;
+  cout << "How long is the list? " << lst.GetLength() << endl;
+  
+  //Use of a dynamic list
+  List* lst1 = new List;
+  cout << "How long is the list?" << lst1->GetLength() << endl;
+  cout << "adding item to list..." << endl;
+  for (int i = 0; i < 5; i++)
+		lst1->PutItemH(10*i);
+  cout << "New list: ";
+  lst1->Print();
+  cout << "How long is the list?" << lst1->GetLength() << endl;
+  cout << endl;
+
+  delete lst1; //necessary to invoke destructor on dynamic list
+*///
+ 
+ 
+/*/// Test 5 - Find
+	cout << "Test Find" << endl;
+  //Use of a static list 
+  List lst;
+  cout << "Where is \"2\" in the list? " << lst.Find(2) << endl;
+  cout << "adding items to list..." << endl;
+  cout << "List: ";
+  for (int i = 0; i < 5; i++) 
+		lst.PutItemH(i);
+ 	lst.Print();
+  cout << endl;
+ 	cout << "Where is \"2\" in the list? " << lst.Find(2) << endl;
+  
+ 	//Use of a dynamic list
+ 	List* lst1 = new List;
+ 	cout << "Where is \"20\" in the list? " << lst1->Find(20) << endl;
+ 	cout << "adding item to list..." << endl;
+ 	for (int i = 0; i < 5; i++)
+ 	  lst1->PutItemH(10*i);
+	cout << "New list: ";
+ 	lst1->Print();
+ 	cout << "Where is \"20\" in the list? " << lst1->Find(20) << endl;
+ 	cout << endl;
+
+ 	delete lst1; //necessary to invoke destructor on dynamic list
+*///
+
+ 
+/// Test 6 - Delete Item
+ 	cout << "Test DeleteItem" << endl;
+ 	//Use of a static list 
+ 	List lst;
+ 	cout << "List: ";
+ 	for (int i = 0; i < 5; i++) 
+   	lst.PutItemH(i);
+ 	lst.Print();
+ 	cout << endl;
+ 	cout << "Deleting item 2..." << endl;
+ 	lst.DeleteItem(2);
+ 	cout << "New List: ";
+ 	lst.Print();
+  
+ 	//Use of a dynamic list
+ 	List* lst1 = new List;
+ 	cout << "List: ";
+ 	for (int i = 0; i < 5; i++)
+   	lst1->PutItemH(10*i);
+ 	lst1->Print();
+ 	cout << "Deleting item w... " << endl;
+ 	lst1->DeleteItem(2);
+ 	cout << "New List: ";
+ 	lst1->Print();
+ 	cout << endl;
+
+ 	delete lst1; //necessary to invoke destructor on dynamic list
+///
+ 
+ 
+ cout << endl;
+ return 0;
+}
